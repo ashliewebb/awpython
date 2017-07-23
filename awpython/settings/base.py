9bce92b193cdb496c96b1f3237ab9753a3d9cabf
Ashlie Webb <greetings@ashliewebb.com>
2017-07-23 18:51:54 +1000

    Allow all hosts

--- awpython/settings/base.py
+++ awpython/settings/base.py
@@ -15,6 +15,8 @@ from __future__ import absolute_import, unicode_literals
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 
+ALLOWED_HOSTS = ['*']
+
 PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 BASE_DIR = os.path.dirname(PROJECT_DIR)
 
