fd84052e56a589a403a7508d3dd5782aac0881ad
Ashlie Webb <greetings@ashliewebb.com>
2017-07-31 21:20:43 +1000

    Tidy stream field implementation on blog
    Add intro to homepage
    Fix RichTextField deps call

--- home/models.py
+++ home/models.py
@@ -3,13 +3,15 @@ from __future__ import absolute_import, unicode_literals
 from django.db import models
 
 from wagtail.wagtailcore.models import Page
-from wagtail.wagtailadmin.edit_handlers import RichTextField
+from wagtail.wagtailcore.fields import RichTextField
 from wagtail.wagtailadmin.edit_handlers import FieldPanel
 
 
 class HomePage(Page):
+    intro = RichTextField(blank=True)
     body = RichTextField(blank=True)
 
     content_panels = Page.content_panels + [
-        FieldPanel('body', classname="full")
+        FieldPanel('intro'),
+        FieldPanel('body', classname='full')
     ]
