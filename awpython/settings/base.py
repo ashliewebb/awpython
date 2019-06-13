394d27510bd3422d2fe31004b1fb1a82fda7879c
Ashlie Webb <greetings@ashliewebb.com>
2019-06-13 21:53:02 +1000

    Deploy attempt

--- awpython/settings/base.py
+++ awpython/settings/base.py
@@ -149,6 +149,8 @@ MEDIA_URL = '/media/'
 # Wagtail settings
 
 WAGTAIL_SITE_NAME = "awpython"
+WAGTAIL_APPEND_SLASH = False
+WAGTAIL_USER_TIME_ZONES = ['Australia/Sydney']
 
 # Base URL to use when referring to full URLs within the Wagtail admin backend -
 # e.g. in notification emails. Don't include '/admin' or a trailing slash
